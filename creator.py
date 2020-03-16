c4800f59ced653b34b90cd98381889d1af126e61
KomarJazdek <49722192+KomarJazdek@users.noreply.github.com>
2020-03-16 08:03:25 +0100

    Update creator.py

--- creator.py
+++ creator.py
@@ -1,6 +1,5 @@
-#/usr/bin/python3
-#pre-alpha version of Open Web
-#This is a software for making websites without web technologies knowledge
+#Pre-alpha of Open Web
+#Open Source webpage creator
 
 wrt = open('index.html','w')
 
@@ -16,15 +15,24 @@ body = "</head> <body>"
 
 step1 = first+title1+title+title2+body
 wrt.write(step1)
-count = 0
-while count < 10:
- textinp = input("Line of the text:")
- textinp += "<br>"
- count += 1
- wrt.write(textinp)
 
+fontsize = input("Type size of a font(1 is the biggest)")
 
+try:
+ while True:
+  textinp = input("Line of the text:")
+  font = "<h" + fontsize + ">"
+  text = font + textinp
+  text += "</h"+fontsize+">"
+  wrt.write(text)
+except:
+ font = "<h" + fontsize + ">"
+ text = font + textinp
+ text += "</h"+fontsize+">"
+ text += "<br>"
+ wrt.write(text)
+ 
+ 
 step2 = "</body> </html>"
 wrt.write(step2)
 wrt.close()
-
