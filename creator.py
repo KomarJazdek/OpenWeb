7c0d09bd3f54e4e7b4478633a60c813fc50cf3a9
KomarJazdek <49722192+KomarJazdek@users.noreply.github.com>
2020-04-13 12:58:10 +0200

    Add files via upload

---
+++ creator.py
@@ -0,0 +1,60 @@
+#Open Source webpage creator
+
+wrt = open('index.html','w')
+
+print("First, give your website a title")
+title = input("Title:")
+
+
+first = "<html> <head>"
+title1 = "<title>"
+title2 = "</title>"
+body = "</head> <body>"
+script = ""
+style = ""
+
+nnc = input("Do you want to add a logo?(y/n)")
+
+color = "white"
+
+if nnc == "y":
+ nnc2 = input("URL/path:")
+ width = input("Width:")
+ height = input("Height:")
+ img = "<img src=\""+nnc2+"\" width=\""+width+"\" height=\""+height+"> <br>"
+indextitle = input("Title to be showed on the center of this page")
+
+title31 = "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    <b id=\"title\">"+indextitle+"</b> "
+
+scndchoice = input("The default background is in white color.Do you want to change color(1)?If not, press any other char")
+
+if scndchoice == "1":
+ color = input("Color:")
+ style = "<style> body {background-color:"+color+";}  </style>"
+
+hr = "<hr style=\"color:"+color+"\>"
+
+step1 = first+title1+title+title2+body+style+img+hr+"<br>"+title31
+wrt.write(step1)
+
+
+fontsize = input("Type size of a font(1 is the biggest,6-the smallest)")
+
+try:
+ while True:
+  textinp = input("Line of the text:")
+  font = "<h" + fontsize + ">"
+  text = font + textinp
+  text += "</h"+fontsize+">"
+  wrt.write(text)
+except:
+ font = "<h" + fontsize + ">"
+ text = font + textinp
+ text += "</h"+fontsize+">"
+ text += "<br>"
+ wrt.write(text)
+ 
+ 
+step2 = "</body> </html>"
+wrt.write(step2)
+wrt.close()
